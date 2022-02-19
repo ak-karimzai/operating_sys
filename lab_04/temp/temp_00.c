bdafcdcf0dc55a50fd50d6d7df242ccb79e12311
Ahmad Khalid Karimzai <ak.karimzai@mail.ru>
2022-02-19 10:09:04 +0300

    lab_01 sem02

--- lab_04/temp/temp_00.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-
-int main() {
-    char *const arr[] = {"<", "text.txt", "c", NULL};
-
-    execv("./02.exe", arr);
-    return 0;
-}
--- lab_04/temp/temp_01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-    char c;
-    while ((c = getchar()) != '\n') {
-        putc(c, stdout);
-    }
-    return 0;
-}
