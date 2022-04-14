420af906cb8d01cbddd73d98b21c5100efc6024e
Ahmad Khalid Karimzai <ak.karimzai@mail.ru>
2022-04-14 22:24:26 +0300

    lab_04 part 01

--- os_02/lab_04/src/part_01/main.c
+++ os_02/lab_04/src/part_01/main.c
@@ -220,63 +220,6 @@ void task_info(const char *proc_path, FILE *out)
     closedir(dp);
 }
 
-void print_page(uint64_t address, uint64_t data, FILE *out)
-{
-    fprintf(out, "0x%-16lx : %-16lx %-10ld %-10ld %-10ld %-10ld\n", address,
-                 data & (((uint64_t)1 << 55) - 1), (data >> 55) & 1,
-                (data >> 61) & 1, (data >> 62) & 1, (data >> 63) & 1);
-}
-
-void get_pagemap_info(const char *proc, FILE *out)
-{
-    fprintf(out, "PAGEMAP\n");
-    fprintf(out, " addr : pfn soft-dirty file/shared swapped present\n");
-
-    char path[BUFSIZ];
-    snprintf(path, BUFSIZ, "%s/maps", proc);
-    FILE *maps = fopen(path, "r");
-
-    snprintf(path, BUFSIZ, "%s/pagemap", proc);
-    int pm_fd = open(path, O_RDONLY);
-
-    char buf[BUFSIZ + 1] = "\0";
-    int len;
-
-    while ((len = fread(buf, 1, BUFSIZ, maps)) > 0)
-    {
-        for (int i = 0; i < len; ++i)
-            if (buf[i] == 0)
-                buf[i] = '\n';
-        buf[len] = '\0';
-
-        char *save_row;
-        char *row = strtok_r(buf, "\n", &save_row);
-
-        while (row)
-        {
-            char *addresses = strtok(row, " ");
-            char *start_str, *end_str;
-
-            if ((start_str = strtok(addresses, "-")) && (end_str = strtok(NULL, "-")))
-            {
-                uint64_t start = strtoul(start_str, NULL, 16);
-                uint64_t end = strtoul(end_str, NULL, 16);
-
-                for (uint64_t i = start; i < end; i += sysconf(_SC_PAGE_SIZE))
-                {
-                    uint64_t offset;
-                    uint64_t index = i / sysconf(_SC_PAGE_SIZE) + sizeof(offset);
-
-                    pread(pm_fd, &offset, sizeof(offset), index);
-                    print_page(i, offset, out);
-                }
-            }
-            row = strtok_r(NULL, "\n", &save_row);
-        }
-    }
-    fclose(maps);
-    close(pm_fd);
-}
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -304,4 +247,6 @@ int main(int argc, char **argv)
     task_info(link_ptr, out);
     pagemap_info(link_ptr, out);
     fclose(out);
+    
+    return 0;
 }
